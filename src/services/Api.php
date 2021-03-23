e1cc6da6624c8684f5e4b6b444eb70c342b7c51d
Meldgaard <cm@quantity.dk>
2021-03-23 19:00:03 +0100

    Fixed errors when creating invoices

--- src/services/Api.php
+++ src/services/Api.php
@@ -12,87 +12,88 @@ use QD\commerce\economic\models\Product;
 class Api extends Component
 {
 
-    public $client;
+	public $client;
 
-    // Public Methods
-    // =========================================================================
+	// Public Methods
+	// =========================================================================
 
-    public function __construct()
-    {
-        $settings = Economic::getInstance()->getSettings();
-        $grantToken = Craft::parseEnv($settings->grantToken);
-        $secretToken = Craft::parseEnv($settings->secretToken);
-        $this->client = new RestClient($secretToken, $grantToken);
-    }
+	public function __construct()
+	{
+		$settings = Economic::getInstance()->getPlugin()->getSettings();
+		$grantToken = Craft::parseEnv($settings->grantToken);
+		$secretToken = Craft::parseEnv($settings->secretToken);
+		$this->client = new RestClient($secretToken, $grantToken);
+	}
 
 
-    //Move out into own seperate services
-    public function getAllProductGroups()
-    {
-        $response = $this->client->request->get('product-groups');
+	//Move out into own seperate services
+	public function getAllProductGroups()
+	{
+		$response = $this->client->request->get('product-groups');
 
-        $status = $response->httpStatus();
+		$status = $response->httpStatus();
 
-        if ($status == 200) {
-            return $response;
-        }
+		if ($status == 200) {
+			return $response;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public function getAllPaymentTerms()
-    {
-        $response = $this->client->request->get('payment-terms');
+	public function getAllPaymentTerms()
+	{
+		$response = $this->client->request->get('payment-terms');
 
-        $status = $response->httpStatus();
+		$status = $response->httpStatus();
 
-        if ($status == 200) {
-            return $response;
-        }
+		if ($status == 200) {
+			return $response;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public function getAllLayouts()
-    {
-        $response = $this->client->request->get('layouts');
+	public function getAllLayouts()
+	{
+		$response = $this->client->request->get('layouts');
 
-        $status = $response->httpStatus();
+		$status = $response->httpStatus();
 
-        if ($status == 200) {
-            return $response;
-        }
+		if ($status == 200) {
+			return $response;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public function getAllVatZones()
-    {
-        $response = $this->client->request->get('vat-zones');
+	public function getAllVatZones()
+	{
+		$response = $this->client->request->get('vat-zones');
 
-        $status = $response->httpStatus();
+		$status = $response->httpStatus();
 
-        if ($status == 200) {
-            return $response;
-        }
+		if ($status == 200) {
+			return $response;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public function syncVariant(Product $variant)
-    {
-        $testResponse = $this->client->request->get('products/' . $variant->productNumber);
-        if ($testResponse->httpStatus() == 200) {
-            $response = $this->client->request->put('products/' . $variant->productNumber, $variant->asArray());
-        } else {
-            $response = $this->client->request->post('products', $variant->asArray());
-        }
+	public function syncVariant(Product $variant)
+	{
+		$testResponse = $this->client->request->get('products/' . urlencode($variant->productNumber));
 
-        $status = $response->httpStatus();
-        if ($status == 201 || $status == 200) {
-            return $response;
-        }
+		if ($testResponse->httpStatus() == 200) {
+			$response = $this->client->request->put('products/' . urlencode($variant->productNumber), $variant->asArray());
+		} else {
+			$response = $this->client->request->post('products', $variant->asArray());
+		}
 
-        Log::error(\implode(',', $response->asArray()['errors']));
-    }
+		$status = $response->httpStatus();
+		if ($status == 201 || $status == 200) {
+			return $response;
+		}
+
+		Log::error(\implode(',', $response->asArray()['errors']));
+	}
 }
--- src/services/Orders.php
+++ src/services/Orders.php
@@ -13,140 +13,143 @@ use craft\commerce\records\Transaction as TransactionRecord;
 
 class Orders extends Component
 {
-    public function addAutoCaptureJob($event)
-    {
-        $order = $event->order;
-        $orderstatus = $order->getOrderStatus();
-        $gateway = $order->getGateway();
+	public function addAutoCaptureJob($event)
+	{
+		$order = $event->order;
+		$orderstatus = $order->getOrderStatus();
+		$gateway = $order->getGateway();
 
-        if ($gateway instanceof Ean && $gateway->autoCapture && $gateway->autoCaptureStatus === $orderstatus->handle) {
-            $transaction = $this->getSuccessfulTransactionForOrder($order);
+		if ($gateway instanceof Ean && $gateway->autoCapture && $gateway->autoCaptureStatus === $orderstatus->handle) {
+			$transaction = $this->getSuccessfulTransactionForOrder($order);
 
-            if ($transaction && $transaction->canCapture()) {
-                Craft::$app->getQueue()->delay(10)->push(new CapturePayment(
-                    [
-                        'transaction' => $transaction,
-                    ]
-                ));
-            }
-        }
-    }
+			if ($transaction && $transaction->canCapture()) {
+				Craft::$app->getQueue()->delay(10)->push(new CapturePayment(
+					[
+						'transaction' => $transaction,
+					]
+				));
+			}
+		}
+	}
 
-    public function getSuccessfulTransactionForOrder(Order $order)
-    {
-        $transactions = $order->getTransactions();
-        usort($transactions, array($this, 'dateCompare'));
+	public function getSuccessfulTransactionForOrder(Order $order)
+	{
+		$transactions = $order->getTransactions();
+		usort($transactions, array($this, 'dateCompare'));
 
-        foreach ($transactions as $transaction) {
+		foreach ($transactions as $transaction) {
 
-            if (
-                $transaction->status === TransactionRecord::STATUS_SUCCESS
-                && $transaction->type === TransactionRecord::TYPE_AUTHORIZE
-            ) {
-                return $transaction;
-            }
-        }
+			if (
+				$transaction->status === TransactionRecord::STATUS_SUCCESS
+				&& $transaction->type === TransactionRecord::TYPE_AUTHORIZE
+			) {
+				return $transaction;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public function getTaxRatesForOrder($order)
-    {
-        $adjustments = $order->getAdjustments();
-        $taxRates = [];
-        foreach ($adjustments as $adjustment) {
-            if ($adjustment->type === 'tax') {
-                return $adjustment->sourceSnapshot;
-            }
-        }
-    }
+	public function getTaxRatesForOrder($order)
+	{
+		$adjustments = $order->getAdjustments();
+		$taxRates = [];
+		foreach ($adjustments as $adjustment) {
+			if ($adjustment->type === 'tax') {
+				return $adjustment->sourceSnapshot;
+			}
+		}
+	}
 
-    /**
-     * Return all order lineitems as e-conomic array
-     *
-     * @param \craft\commerce\elements\Order $order
-     *
-     * @return array;
-     */
-    public function getOrderLines(Order $order)
-    {
-        $orderLines = $order->getLineItems();
-        $lines = [];
+	/**
+	 * Return all order lineitems as e-conomic array
+	 *
+	 * @param \craft\commerce\elements\Order $order
+	 *
+	 * @return array;
+	 */
+	public function getOrderLines(Order $order)
+	{
+		$orderLines = $order->getLineItems();
+		$lines = [];
 
-        //Add order line items
-        foreach ($orderLines as $orderLine) {
-            $price = $orderLine->price;
-            $discountAmount = 0;
+		//Add order line items
+		foreach ($orderLines as $orderLine) {
+			$price = $orderLine->price;
+			$discountAmount = 0;
+			$vatDecimal = 1;
 
-            $adjustments = $orderLine->getAdjustments();
+			$adjustments = $orderLine->getAdjustments();
 
-            foreach ($adjustments as $adjustment) {
-                if ($adjustment->included && $adjustment->type !== 'tax') {
-                    continue;
-                }
+			foreach ($adjustments as $adjustment) {
+				if ($adjustment->included && $adjustment->type !== 'tax') {
+					continue;
+				}
 
-                if (!$adjustment->included && $adjustment->type === 'tax') {
-                    continue;
-                }
+				if (!$adjustment->included && $adjustment->type === 'tax') {
+					continue;
+				}
 
-                if ($adjustment->included && $adjustment->type === 'tax') {
-                    $price -= $adjustment->amount;
-                    continue;
-                }
+				if ($adjustment->included && $adjustment->type === 'tax') {
+					$vatDecimal = $adjustment->amount / (($orderLine->subtotal * $orderLine->qty) - $adjustment->amount);
+					$price -= $adjustment->amount;
+					continue;
+				}
 
-                if ($adjustment->amount < 0) {
-                    $discountAmount -= $adjustment->amount;
-                    continue;
-                }
+				if ($adjustment->amount < 0) {
+					$discountAmount -= $adjustment->amount;
+					continue;
+				}
 
-                if ($adjustment->amount > 0) {
-                    $price += $adjustment->amount;
-                    continue;
-                }
-            }
+				if ($adjustment->amount > 0) {
+					$price += $adjustment->amount;
+					continue;
+				}
+			}
 
-            $lines[] = [
-                "product" => [
-                    "productNumber" => $orderLine->purchasable->sku
-                ],
-                "description" => $orderLine->description,
-                "quantity" => $orderLine->qty,
-                "discountAmount" => $discountAmount,
-                "unitNetPrice" => $price,
-            ];
-        }
+			$lines[] = [
+				"product" => [
+					"productNumber" => $orderLine->purchasable->sku
+				],
+				"description" => $orderLine->description,
+				"quantity" => $orderLine->qty,
+				"discountAmount" => $discountAmount,
+				"unitNetPrice" => $price,
+			];
+		}
 
-        //Add shipping line items
-        $adjustments = $order->getAdjustments();
-        $shippingRelations = Json::decode(Economic::getInstance()->getSettings()->gatewayPaymentTerms);
-        $shippingProductNumber = null;
-        foreach ($shippingRelations as $shippingRelation) {
-            if ($shippingRelation[0] == $order->getShippingMethod()->id) {
-                $shippingProductNumber = $shippingRelation[1];
-            }
-        }
+		//Add shipping line items
+		$adjustments = $order->getAdjustments();
+		$shippingRelations = Json::decode(Economic::getInstance()->getSettings()->shippingProductnumbers);
+		$shippingProductNumber = null;
+		foreach ($shippingRelations as $shippingRelation) {
+			if ($shippingRelation[0] == $order->getShippingMethod()->id) {
+				$shippingProductNumber = $shippingRelation[1];
+			}
+		}
 
-        if ($shippingProductNumber) {
-            foreach ($adjustments as $adjustment) {
-                if ($adjustment->type === 'shipping') {
-                    $lines[] = [
-                        "product" => [
-                            "productNumber" => $shippingProductNumber
-                        ],
-                        "quantity" => 1,
-                        "unitNetPrice" => $adjustment->amount,
-                    ];
-                }
-            }
-        }
+		if ($shippingProductNumber) {
+			foreach ($adjustments as $adjustment) {
+				if ($adjustment->type === 'shipping') {
+					$lines[] = [
+						"product" => [
+							"productNumber" => $shippingProductNumber
+						],
+						"description" => 'Shipping',
+						"quantity" => 1,
+						"unitNetPrice" => $adjustment->amount / ($vatDecimal + 1),
+					];
+				}
+			}
+		}
 
-        return $lines;
-    }
+		return $lines;
+	}
 
-    private static function dateCompare($element1, $element2)
-    {
-        $datetime1 = date_timestamp_get($element1['dateCreated']);
-        $datetime2 = date_timestamp_get($element2['dateCreated']);
-        return $datetime2 - $datetime1;
-    }
+	private static function dateCompare($element1, $element2)
+	{
+		$datetime1 = date_timestamp_get($element1['dateCreated']);
+		$datetime2 = date_timestamp_get($element2['dateCreated']);
+		return $datetime2 - $datetime1;
+	}
 }
